6f8f84f508bc4592c73e89a80d9a910aa3e3ea69
ivansukhoparov <rbcdaaa@gmail.com>
2023-10-13 23:28:39 +0200

    update: refractoring code for task -Beginner - Reduce but Grow-

--- 003-reduceButGrow.js
+++ 003-reduceButGrow.js
@@ -1,8 +1,6 @@
 //function given a non-empty array of integers, return the result of multiplying the values together in order.
 function grow(x) {
     let el = 1;
-    for (let i = 0; i < x.length; i++) {
-        el = el * x[i]
-    };
+    x.forEach((i) => { el = el * i; });
     return el;
 }
