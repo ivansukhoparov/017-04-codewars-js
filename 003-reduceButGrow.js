c8f4632d3bd0b5d756a5b83bd348b2d4eb99a5d2
ivansukhoparov <rbcdaaa@gmail.com>
2023-10-13 23:24:06 +0200

    done: task -Beginner - Reduce but Grow- is done

---
+++ 003-reduceButGrow.js
@@ -0,0 +1,8 @@
+//function given a non-empty array of integers, return the result of multiplying the values together in order.
+function grow(x) {
+    let el = 1;
+    for (let i = 0; i < x.length; i++) {
+        el = el * x[i]
+    };
+    return el;
+}
