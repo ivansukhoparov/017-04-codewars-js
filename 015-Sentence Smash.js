ac498d278aef9e82c8e3b33a93c0230760c98b80
ivansukhoparov <rbcdaaa@gmail.com>
2023-10-26 00:16:36 +0200

    done: -Sentence Smash- JS codewars.com task is done

---
+++ 015-Sentence Smash.js
@@ -0,0 +1,20 @@
+/*
+Sentence Smash
+
+Sentence Smash
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+*/
+
+function smash(words) {
+    let sentence = ''
+    words.forEach((word, index) => {
+        sentence += word; // add word to sentence variable
+        if (index < words.length - 1) { // check the variable 'word' is the last one in the array and add space between words if no 
+            sentence += ' ';
+        }
+    })
+    return sentence;
+};
