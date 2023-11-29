17eb1668b665788d36ba8e3ef3d51860eafdfc54
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-29 23:24:38 +0100

    complete task "Reversed Strings"

---
+++ 024- Reversed Strings.js
@@ -0,0 +1,18 @@
+/*
+
+Reversed Strings
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+ */
+
+function solution(str){
+    let res = [];
+    for (let i=str.length-1; i>=0; i--) {
+        res.push(str[i]);
+    }
+    return res.join("");
+}
