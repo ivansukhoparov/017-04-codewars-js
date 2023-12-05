55bfb00c8a99be7a591ee6b09922c5bf7034805b
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-05 23:03:06 +0100

    task "Highest and Lowest" new solution

--- 027-Highest_and_Lowest.js
+++ 027-Highest_and_Lowest.js
@@ -22,4 +22,10 @@ function highAndLow(numbers) {
     return `${n[0]} ${n[n.length-1]}`
 }
 
+
+function highAndLow2(numbers){
+    numbers = numbers.split(' ');
+    return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+
 console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
