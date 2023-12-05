b956c98f93d7fa5f9040639151fbbf4f512307b4
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-05 19:58:05 +0100

    task "Highest and Lowest"

---
+++ 027-Highest_and_Lowest.js
@@ -0,0 +1,25 @@
+/*
+Highest and Lowest
+
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+Notes
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+ */
+
+function highAndLow(numbers) {
+    const n = numbers.split(" ").sort((a, b) => {
+        if (+a < +b) return 1
+        else if (+a > +b) return -1
+        else return 0
+    })
+    return `${n[0]} ${n[n.length-1]}`
+}
+
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
