03718429d407fcefd7e267527ff01f4be0e42a96
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-08 23:47:48 +0100

    task "I love you, a little , a lot, passionately ... not at all" new solution

--- 029-I love you, a little , a lot, passionately not at all.js
+++ 029-I love you, a little , a lot, passionately not at all.js
@@ -30,3 +30,16 @@ function howMuchILoveYou(nbPetals) {
 }
 
 
+function howMuchILoveYou_2(nbPetals) {
+    let phrase = {
+        0: "not at all",
+        1: "I love you",
+        2: "a little",
+        3: "a lot",
+        4: "passionately",
+        5: "madly"
+    }
+    return phrase[nbPetals%6]
+}
+
+
